9715122deb93aafdac63c6277fdc974176928aae
w3cdp6084-dev <yusukew3@gmail.com>
2023-11-20 11:14:29 +0900

    notion data 取得

---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps } from 'next';
+import { Client } from '@notionhq/client';
+import { remark } from 'remark'
+import html from 'remark-html';
+
+// Notion APIクライアントの初期化
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
+// マークダウンからHTMLに変換する関数
+const markdownToHtml = async (markdown: string) => {
+  const result = await remark().use(html).process(markdown);
+  return result.toString();
+};
+
+// Notionからブログ投稿を取得する関数
+const getPosts = async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+
+  if (!databaseId) {
+    throw new Error("NOTION_DATABASE_ID is not defined");
+  }
+
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+
+  // レスポンスからブログ投稿のデータを抽出し、必要な変換を行う
+  const posts = await Promise.all(
+    response.results.map(async (page) => {
+      // ページがPageObjectResponse型であることを確認
+      if ('properties' in page) {
+        // ここでは、ページのタイトルと日付を取得する例を示します
+        const titleProperty = page.properties['Title'];
+        const dateProperty = page.properties['Date'];
+
+        let title = '';
+        let date = '';
+
+        if (titleProperty && titleProperty.type === 'title' && titleProperty.title.length > 0) {
+          title = titleProperty.title[0].plain_text;
+        }
+
+        if (dateProperty && dateProperty.type === 'date' && dateProperty.date) {
+          date = dateProperty.date.start;
+        }
+
+        return { id: page.id, title, date };
+      }
+    })
+  );
+
+  // undefinedの要素をフィルタリング
+  return posts.filter(Boolean);
+};
+
+// getStaticPropsを使用してビルド時にブログ投稿を取得
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+  return { props: { posts } };
+};
+
+// ページコンポーネント
+const Home = ({ posts }) => {
+  return (
+    <div>
+      <h1>ブログ</h1>
+      {posts.map((post) => (
+        <article key={post.id}>
+          <h2>{post.title}</h2>
+          <div>{post.date}</div>
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
