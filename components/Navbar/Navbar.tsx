3aa3dfe0ae2c694ea01341a19d7e23c8282780bf
w3cdp6084-dev <yusukew3@gmail.com>
2024-02-23 14:24:33 +0900

    時間が経過してるので一旦コミット

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      gsap.to(menuRef.current, { x: '0%', duration: 0.5 });
+    } else {
+      gsap.to(menuRef.current, { x: '100%', duration: 0.5 });
+    }
+  }, [isOpen]);
 
   return (
     <nav>
@@ -17,27 +27,25 @@ const Navbar = () => {
         <button onClick={toggleMenu}>
           <span>☰</span>
         </button>
-        {isOpen && (
-          <div>
-            <ul>
-              <li>
-                <Link href="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href="#">
-                  Twitter
-                </Link>
-              </li>
-              <li>
-                <Link href="#">
-                  Qiita
-                </Link>
-              </li>
-            </ul>
-          </div>
-        )}
+        <div ref={menuRef} style={{ x: '100%', position: 'fixed', top: 0, right: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.8)', color: '#fff', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', transition: 'all 0.5s ease' }}>
+          <ul>
+            <li>
+              <Link href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                Twitter
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                Qiita
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
