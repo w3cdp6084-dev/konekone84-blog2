2640e8a17c089f82ee5e704c983ac9c1106869af
w3cdp6084-dev <yusukew3@gmail.com>
2024-03-07 21:30:22 +0900

    footer仮入れ

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h1>Footer</h1>
+        </div>
+      </div>
     </div>
   )
 }
