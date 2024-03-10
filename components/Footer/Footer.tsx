707ab305e8ebb0c498801c5b9bf89524d7a10e1e
w3cdp6084-dev <yusukew3@gmail.com>
2024-03-10 12:26:44 +0900

    Footer styled

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>@2024 Yusuke Mori</h1>
-        </div>
-      </div>
+    <div className={styles.outer}>
+      <footer className={styles.footerWrap}>
+        <ul>
+          <Link href="https://twitter.com/konekone"><li>Twitter</li></Link>
+          <Link href="https://www.facebook.com/yusuke.mori.9"><li>Facebook</li></Link>
+          <Link href="https://github.com/konekone"><li>GitHub</li></Link>
+          <li>Dribbble</li>
+        </ul>
+        <h1>@2024 Yusuke Mori</h1>
+      </footer>
     </div>
   )
 }
